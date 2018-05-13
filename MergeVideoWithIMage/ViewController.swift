cb3a893116f7db714b61a5a84cae6626d48bd9b1
ajaynathms <ajaynath.ms@cognizant.com>
2018-05-13 15:12:18 +0530

    Initial Commit

---
+++ MergeVideoWithIMage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MergeVideoWithIMage
+//
+//  Created by Ajaynath MS on 13/05/18.
+//  Copyright © 2018 ajay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
